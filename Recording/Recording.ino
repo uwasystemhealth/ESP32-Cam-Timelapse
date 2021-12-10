f9cd9d2db6f866fb285a10667eaa7fa6f0b58b4e
Ben Travaglione <ben@travaglione.com>
2021-12-10 16:25:56 +0800

    Setting now working. Image still green

--- Recording/Recording.ino
+++ Recording/Recording.ino
@@ -89,29 +89,6 @@ void setup() {
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG; 
 
-  //**Camera Effects**
-  //sensor_t * s = esp_camera_sensor_get();
-  //s->set_quality(s, 10);
-  //s->set_brightness(s, 0);
-  //s->set_contrast(s, 0);
-  //s->set_saturation(s, 1);
-  //s->set_sharpness(s, 0);
-  //s->set_wb_mode(s, 0);
-  //s->set_awb_gain(s, 1);
-  //s->set_aec_value(s, 1);
-  //s->set_aec2(s, 0);
-  //s->set_ae_level(s, 0);
-  //s->set_aec_value(s, 168);
-  //s->set_agc_gain(s, 0);
-  //s->set_gainceiling(s, (gainceiling_t)1); 
-  //s->set_bpc(s, 0);
-  //s->set_wpc(s, 1);
-  //s->set_raw_gma(s, 1);
-  //s->set_lenc(s, 1);
-  //s->set_vflip(s, 0);
-  //s->set_hmirror(s, 0); 
-  //s->set_whitebal(s, 1);
-  //s->set_dcw(s, 1);
   
   // Camera Quality assuming 3.8GB of usable space
   // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
@@ -127,6 +104,31 @@ void setup() {
   // Init Camera
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {Serial.printf("Camera init failed with error 0x%x", err);return;}
+
+sensor_t * s = esp_camera_sensor_get();
+
+s->set_brightness(s, 0);     // -2 to 2
+s->set_contrast(s, 0);       // -2 to 2
+s->set_saturation(s, 0);     // -2 to 2
+s->set_special_effect(s, 0); // 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
+s->set_whitebal(s, 1);       // 0 = disable , 1 = enable
+s->set_awb_gain(s, 1);       // 0 = disable , 1 = enable
+s->set_wb_mode(s, 0);        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+s->set_exposure_ctrl(s, 1);  // 0 = disable , 1 = enable
+s->set_aec2(s, 0);           // 0 = disable , 1 = enable
+s->set_ae_level(s, 0);       // -2 to 2
+s->set_aec_value(s, 300);    // 0 to 1200
+s->set_gain_ctrl(s, 1);      // 0 = disable , 1 = enable
+s->set_agc_gain(s, 0);       // 0 to 30
+s->set_gainceiling(s, (gainceiling_t)0);  // 0 to 6
+s->set_bpc(s, 0);            // 0 = disable , 1 = enable
+s->set_wpc(s, 1);            // 0 = disable , 1 = enable
+s->set_raw_gma(s, 1);        // 0 = disable , 1 = enable
+s->set_lenc(s, 1);           // 0 = disable , 1 = enable
+s->set_hmirror(s, 0);        // 0 = disable , 1 = enable
+s->set_vflip(s, 0);          // 0 = disable , 1 = enable
+s->set_dcw(s, 1);            // 0 = disable , 1 = enable
+s->set_colorbar(s, 0);       // 0 = disable , 1 = enable
   
   // Init SD Card
   if(!SD_MMC.begin()){
